25dbda1eebfc0df75531a94270bc7870dc14c339
b.machado <bruno_dinizmachado@yahoo.com.br>
2020-11-12 16:43:50 -0300

    teste de commit 2

--- app/src/main/java/com/example/redivivus/MainActivity.java
+++ app/src/main/java/com/example/redivivus/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.redivivus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+
+public class MainActivity extends AppCompatActivity/* implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, ActivityCompat.OnRequestPermissionsResultCallback*/{
+
+    /*public static boolean LOCATION_PERMISSION_CODE= false;
+    public GoogleMap mapa;
+    public LatLng localizacao = new LatLng(-23.951137, -46.339025);
+    private Button btMinhaPosicao;
+    private GeoDataClient geoDataClient;
+    private FusedLocationProviderClient mfusedLocationProviderClient;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.nossoMapa);
+        mapFragment.getMapAsync(MainActivity.this);
+        geoDataClient = Places.getGeoDataClient(MainActivity.this, null);
+        mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);*/
     }
+
+    /*@Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(MainActivity.this, "Falha na conexão", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mapa = googleMap;
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(localizacao, 18);
+        mapa.animateCamera(update);
+    }*/
 }

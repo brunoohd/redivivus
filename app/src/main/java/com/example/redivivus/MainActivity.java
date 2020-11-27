41d79a6ccea2950bb7b6c9eb6f9a151e5e577e52
bruno <bruno_dinizmachado@yahoo.com.br>
2020-11-26 22:55:49 -0300

    Limpando o códido

--- app/src/main/java/com/example/redivivus/MainActivity.java
+++ app/src/main/java/com/example/redivivus/MainActivity.java
@@ -1,46 +1,21 @@
 package com.example.redivivus;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.service.carrier.CarrierMessagingService;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.places.GeoDataClient;
-
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
-        //implements
-        //GoogleMap.OnMyLocationButtonClickListener,
-        //GoogleMap.OnMyLocationClickListener,
-        //OnMapReadyCallback,
-        //ActivityCompat.OnRequestPermissionsResultCallback {
 
     private static boolean LOCATION_PERMISSION_REQUEST_CODE = false;
-    //public GoogleMap mapa;
-    //public LatLng localizacao = new LatLng(-23.951137, -46.339025);
     private Button btPermitirLocalizacao, btMapa;
-    //private GeoDataClient geoDataClient;
-    //private FusedLocationProviderClient mfusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +41,12 @@ public class MainActivity extends AppCompatActivity {
             btMapa.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent tela = new Intent(MainActivity.this, MapaActivity.class);
-                    startActivity(tela);
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        Intent tela = new Intent(MainActivity.this, MapaActivity.class);
+                        startActivity(tela);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Sem a permissão a aplicação não irá funcionar", Toast.LENGTH_LONG).show();
+                    }
                 }
             });
         }
@@ -79,86 +58,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-/*
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.nossoMapa);
-        mapFragment.getMapAsync(MainActivity.this);
-        //exemplos da aula
-        //geoDataClient = Places.getGeoDataClient(MainActivity.this, null);
-        //mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        //metodoBotao();
-        //ativaPermissao();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.mapa = googleMap;
-        mapa.setMyLocationEnabled(true);
-        mapa.setOnMyLocationButtonClickListener(this);
-        mapa.setOnMyLocationClickListener(this);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(localizacao, 18);
-        mapa.animateCamera(update);
-    }
-
-    /*private void ativaPermissao(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            LOCATION_PERMISSION_REQUEST_CODE=PermissionUtils.validate(this, 1, Manifest.permission.ACCESS_FINE_LOCATION);
-        }
-
-    }*/
-
-   /* @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
-    }
-
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
-
-    }
-    //botão usado no exemplo da aula
-
-    /*private void metodoBotao(){
-        btMinhaPosicao=(Button)findViewById(R.id.btnPosicao);
-        btMinhaPosicao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                atualizaSuaLocalizacao();
-            }
-        });
-    }
-    //método usado para atualizar localização com o botão usado no exemplo da aula
-    private void atualizaSuaLocalizacao(){
-        try {
-            LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            LocationListener locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                    mapa.addMarker(new MarkerOptions().position(latLng).title("Oi"));
-                    CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, 18);
-                    mapa.animateCamera(update);
-                    mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                    Toast.makeText(MainActivity.this, "Atualizou", Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String s) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String s) {
-
-                }
-            };
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        }
-        catch (SecurityException ex){
-            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
-    }*/
--- app/src/main/java/com/example/redivivus/MapaActivity.java
+++ app/src/main/java/com/example/redivivus/MapaActivity.java
@@ -2,7 +2,7 @@ package com.example.redivivus;
 
 import android.location.Location;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +13,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -27,9 +26,7 @@ public class MapaActivity extends AppCompatActivity  implements
     private static boolean LOCATION_PERMISSION_REQUEST_CODE = false;
     public GoogleMap mapa;
     public LatLng localizacao = new LatLng(-23.5914954, -48.0649624);
-    private Button btPermitirLocalizacao;
-    //private GeoDataClient geoDataClient;
-    //private FusedLocationProviderClient mfusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +34,7 @@ public class MapaActivity extends AppCompatActivity  implements
         setContentView(R.layout.mapa_main);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.nossoMapa);
         mapFragment.getMapAsync(MapaActivity.this);
-        //exemplos da aula
-        //geoDataClient = Places.getGeoDataClient(MainActivity.this, null);
-        //mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        //metodoBotao();
-        //ativaPermissao();
+
     }
 
     @Override
@@ -52,80 +45,44 @@ public class MapaActivity extends AppCompatActivity  implements
         mapa.setOnMyLocationClickListener(this);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(localizacao, 11);
         mapa.animateCamera(update);
-        pontosDescarte();
+        pontosDescarte(); //método para iniciar os marcadores dos pontos de descarte
     }
 
     private void pontosDescarte(){
-        //private static final Rai
+        final LatLng ECOPONTO = new LatLng(-23.5670508, -48.0273684);
+        final LatLng COOPERITA = new LatLng(-23.5684936,-48.0304146);
+        final LatLng RAIA = new LatLng(-23.5858486,-48.0480597);
 
-        mapa.addMarker(new MarkerOptions().position(new LatLng(-23.5670508, -48.0273684)).title("Ecoponto").snippet("Descarte de entulhos, resto de construção, movéis velhos"));
-        mapa.addMarker(new MarkerOptions().position(new LatLng(-23.5684936,-48.0304146)).title("Cooperita - Coop. de Reciclagem de Itapetininga").snippet("Descarte de recicláveis").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-        mapa.addMarker(new MarkerOptions().position(new LatLng(-23.5858486,-48.0480597)).title("Drogaria Raia").snippet("Descarte de medicamentos").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-    }
+        //Marcador do ponto de descarte Ecoponto
+        mapa.addMarker(new MarkerOptions()
+                .position(ECOPONTO)
+                .title("Ecoponto")
+                .snippet("Descarte de entulhos, resto de construção, movéis velhos"));
 
+        //Marcador do ponto de descarte Cooperita
+        mapa.addMarker(new MarkerOptions()
+                .position(COOPERITA)
+                .title("Cooperita - Coop. de Reciclagem de Itapetininga")
+                .snippet("Descarte de recicláveis")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
 
-
-    /*private void ativaPermissao(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            LOCATION_PERMISSION_REQUEST_CODE=PermissionUtils.validate(this, 1, Manifest.permission.ACCESS_FINE_LOCATION);
-        }
-
-    }*/
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
+        //Marcador do ponto de descarte Drogaria Raia
+        mapa.addMarker(new MarkerOptions()
+                .position(RAIA)
+                .title("Drogaria Raia")
+                .snippet("Descarte de medicamentos")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
     }
 
     @Override
     public void onMyLocationClick(@NonNull Location location) {
 
     }
-    //botão usado no exemplo da aula
 
-    /*private void metodoBotao(){
-        btMinhaPosicao=(Button)findViewById(R.id.btnPosicao);
-        btMinhaPosicao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                atualizaSuaLocalizacao();
-            }
-        });
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "Você está aqui", Toast.LENGTH_LONG).show();
+        return false;
     }
-    //método usado para atualizar localização com o botão usado no exemplo da aula
-    private void atualizaSuaLocalizacao(){
-        try {
-            LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            LocationListener locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                    mapa.addMarker(new MarkerOptions().position(latLng).title("Oi"));
-                    CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, 18);
-                    mapa.animateCamera(update);
-                    mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                    Toast.makeText(MainActivity.this, "Atualizou", Toast.LENGTH_LONG).show();
-                }
 
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String s) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String s) {
-
-                }
-            };
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        }
-        catch (SecurityException ex){
-            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
-    }*/
 }
